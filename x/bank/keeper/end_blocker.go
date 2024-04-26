6154798cd414e6239f2d5408d7c685d96b93382f
Phuc Ta <vinhphuctadang@gmail.com>
2024-04-26 13:06:01 +0700

    feat: impl emit bank update events

--- x/bank/keeper/end_blocker.go
+++ x/bank/keeper/end_blocker.go
@@ -2,9 +2,9 @@ package keeper
 
 import (
 	"context"
-	"fmt"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/bank/types"
 )
 
 func (k BaseKeeper) EndBlocker(ctx context.Context) error {
@@ -13,6 +13,7 @@ func (k BaseKeeper) EndBlocker(ctx context.Context) error {
 	itr := store.Iterator(nil, nil)
 	defer itr.Close()
 
+	denomToUpdates := map[string][]*types.AccountBalance{}
 	keyCdc, valueCdc := k.Balances.KeyCodec(), k.Balances.ValueCodec()
 	for itr.Valid() {
 		key := itr.Key()
@@ -27,8 +28,21 @@ func (k BaseKeeper) EndBlocker(ctx context.Context) error {
 			panic(err)
 		}
 
-		fmt.Println("address:", p.K1().String(), "denom:", p.K2(), "=> balance:", v.String())
-		itr.Next()
+		denom := p.K2()
+		denomToUpdates[denom] = append(denomToUpdates[denom], &types.AccountBalance{
+			Account: p.K1(),
+			Balance: v,
+		})
 	}
+
+	// events doesn't break consensus so we don't need to sort the map
+	updateEvent := &types.BalanceUpdate{}
+	for denom, updates := range denomToUpdates {
+		updateEvent.Updates = append(updateEvent.Updates, &types.DenomBalanceUpdate{
+			Denom:    denom,
+			Balances: updates,
+		})
+	}
+	sdkCtx.EventManager().EmitTypedEvent(updateEvent)
 	return nil
 }
